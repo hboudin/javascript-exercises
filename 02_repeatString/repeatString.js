40db8daae448dde35918430595948d4e00ca3b05
hboudin <hboudin1@gmail.com>
2025-02-23 17:39:38 +0100

    Complete exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(string, num) {
+    let result = ""
+    for (let i = 1; i <= num; i++) {
+    result += string
+   }
+   if (num < 0) {
+    return "ERROR"
+    } else {
+    return result
+    }
+}
 // Do not edit below this line
 module.exports = repeatString;
