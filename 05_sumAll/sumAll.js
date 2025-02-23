41a854ae9727f3a7b40feffbc9020b287732eb88
hboudin <hboudin1@gmail.com>
2025-02-23 20:49:31 +0100

    Complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let answer = 0
+    
+    if (num1 < 0 || num2 < 0 || Math.floor(num1) !== num1 || Math.floor(num2) !== num2) {
+        return "ERROR"
+    }
+    else if (num1 < num2) {
+        for (let i = num1; i <= num2; i++) {
+            answer += i
+        }
+    } else {
+        for (let i = num2; i <= num1; i++) {
+        answer += i
+    }
+} return answer
 };
 
 // Do not edit below this line
