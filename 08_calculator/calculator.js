03ddc552ed0170eb2e2730c1f9d1bd135c42b268
hboudin <hboudin1@gmail.com>
2025-03-07 18:25:51 +0100

    Complete exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, item) => (total + item), 0)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, item) => (total * item))
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let res = 1
+  for (let i = 1; i <= a; i++) {
+    res *= i
+  } return res
 };
 
 // Do not edit below this line
