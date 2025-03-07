5804c56eb9f98825247156ebb71c50e1fb6103a5
hboudin <hboudin1@gmail.com>
2025-03-07 23:37:31 +0100

    Complete exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,11 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let todaysDate = new Date().getFullYear()
+    arr.map((item) => 
+        item.age = (item.yearOfDeath || todaysDate) - item.yearOfBirth)
 
-};
+    arr.sort((a, b) => b.age - a.age)
+    return arr[0]
+};          
 
 // Do not edit below this line
 module.exports = findTheOldest;
