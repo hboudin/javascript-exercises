b35698d332cda0517b50892fa0c3dedd24da5b0f
hboudin <hboudin1@gmail.com>
2025-03-07 20:42:24 +0100

    Complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let fibArray = [0, 1]
+    
+    if (num >= 0) {
+        for (let i = 2; i <= num; i++) {
+        
+        let nextNum =
+        fibArray[fibArray.length - 1]
+        + fibArray[fibArray.length - 2]
+        
+        fibArray.push(nextNum)
+    }  return fibArray[num]
+    
+    } else {
+        return "OOPS"
+    }
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
