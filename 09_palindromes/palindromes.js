f26c130fd1ae4185c171897b5d23538a4c053b14
hboudin <hboudin1@gmail.com>
2025-03-07 19:47:08 +0100

    Complete exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let arr = str.split("")
+    let sortedArr = arr
+    .filter((item) => /^[a-zA-Z0-9]*$/.test(item))
+    .map((item) => item = item.toLowerCase())
+    
+    
+    let arrReversed = [...sortedArr]
+    arrReversed.reverse()
+    
+    console.log("arrays", sortedArr, "-----", arrReversed)
 
-};
+    
+    ogStr = sortedArr.join()
+    revStr = arrReversed.join()
+
+    console.log("strings", ogStr, "----", revStr)
+
+    if (ogStr === revStr) {
+        return true
+    } else {
+        return false
+    }
+}
 
 // Do not edit below this line
 module.exports = palindromes;
